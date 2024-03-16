b8b3e2a4698b8e023e9ee2e2e9f0098fa9cf4182
Duaa Elsayed <73131139+doaa10@users.noreply.github.com>
2024-03-17 01:01:12 +0200

    files names

---
+++ src/components/large/charts/UserChart/UsersData.ts
@@ -0,0 +1,8 @@
+export const data = [
+     { name: "January", Total: 1200 },
+    { name: "February", Total: 2100 },
+    { name: "March", Total: 800 },
+    { name: "April", Total: 1600 },
+    { name: "May", Total: 900 },
+    { name: "June", Total: 1700 },
+]
---
+++ src/components/small/Widgets/widgetsData.js
@@ -0,0 +1,39 @@
+import { IoTrendingUp } from "react-icons/io5";
+
+export const WidgetsData =[
+{
+    title:"Users",
+    isStatistics:true,
+    link:"see all users",
+    icon:<IoTrendingUp/>,
+    value:"15.200",
+    percentage:'+11.02%'
+},
+{
+    title:"ActiveUsers",
+    isStatistics:true,
+    link:"see all users",
+    value:"9000",
+    icon:<IoTrendingUp/>,
+    percentage:'+6.02%'
+
+},
+{
+    title:"Total Exercises",
+    isStatistics:false,
+    link:"see all Exercises",
+    icon:<IoTrendingUp/>,
+    value:"10.000",
+    percentage:'+6.02%'
+
+},
+{
+    title:" Total Meals",
+    isStatistics:false,
+    link:"see all meals",
+    icon:<IoTrendingUp/>,
+    value:"30.000",
+    percentage:'+6.02%'
+
+},
+]
