700317be84d8859fd75597db4f4e8be870da4c9d
a7med7amed7 <aboz7amed7@gmail.com>
2024-03-30 01:00:38 +0200

    create-admin page

--- src/app/(routes)/create-admin/page.tsx
+++ src/app/(routes)/create-admin/page.tsx
@@ -1,8 +1,81 @@
 import React from 'react'
-
+import styles from './page.module.css';
+import Input from '@/components/small/Inputs/Input';
+import Button from '@/components/small/Button/Button';
 function page() {
   return (
-    <div>page</div>
+    <div className={styles.createAdmin}>
+      <section className={styles.createAdmin__top}>
+        <div className={styles.createAdmin__top__title}>Create an Admin</div>
+      </section>
+      <section className={styles.createAdmin__bottom}>
+        <form className={styles.createAdmin__form}>
+          <Input PlaceHolder='First Name' type='text' size='small' />
+          <Input PlaceHolder='Last Name' type='text' size='small' />
+          <Input PlaceHolder='Creator' type='text' size='small' />
+          <Input PlaceHolder='Email' type='email' size='large' />
+
+          <Input PlaceHolder='Password' type='password' size='large' />
+          <Input PlaceHolder='Confirm Password' type='password' size='large' />
+          <section className={styles.createAdmin__form__auth}>
+            <fieldset className={styles.createAdmin__form__auth__fieldset}>
+              <legend className={styles.createAdmin__form__auth__fieldset__legend}>Choose Authories For Exercises</legend>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Add Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Edit Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>View Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Delete Exercise</label>
+              </div>
+
+            </fieldset>
+          </section>
+          <section className={styles.createAdmin__form__auth}>
+            <fieldset className={styles.createAdmin__form__auth__fieldset}>
+              <legend className={styles.createAdmin__form__auth__fieldset__legend}>Choose Authories For Exercises</legend>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Add Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Edit Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>View Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Delete Exercise</label>
+              </div>
+              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Delete Exercise</label>
+              </div>              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Delete Exercise</label>
+              </div>              <div className={styles.createAdmin__form__auth__fieldset__item}>
+                <input type='checkbox' className={styles.createAdmin__form__auth__fieldset__item__input} />
+                <label className={styles.createAdmin__form__auth__fieldset__item__label}>Delete Exercise</label>
+              </div>
+            </fieldset>
+          </section>
+          <div className={styles.createAdmin__form__wrapper}>
+            <Button type='primary' size='full'>Submit</Button>
+          </div>
+        </form>
+      </section>
+    </div>
   )
 }
 
--- src/components/large/SideDrawer/NavLinks.tsx
+++ src/components/large/SideDrawer/NavLinks.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import styles from './SideDrawer.module.css'
-<<<<<<< HEAD
-import { links } from './Links' 
-=======
 import { links } from './Links'
->>>>>>> 6d46082f8826afb17f3f51726fac9f9639030594
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { IoLogOut } from "react-icons/io5";
@@ -14,33 +10,33 @@ const NavLinks = () => {
     const pathname = usePathname();
     return (
         <div className={styles.CenterLinks}>
-        <div className={styles.sidelinks}>
-            {
-                links.map((link) => {
-                    return <ul key={link.id} >
+            <div className={styles.sidelinks}>
+                {
+                    links.map((link) => {
+                        return <ul key={link.id} >
 
-                        <li className={pathname === link.path ? styles.activeLink : styles.other}>
-                            {link.icon}
-                        
-                            <Link href={link.path}>
-                            {link.title}
+                            <li className={pathname === link.path ? styles.activeLink : styles.other}>
+                                {link.icon}
 
-                            </Link>
-                        </li>
+                                <Link href={link.path}>
+                                    {link.title}
 
-                    </ul>
-                })
-            }
-             <div className={styles.sidebottomlink}>
-                <li >
-                    <IoLogOut size={"23"} />
-                    <a>Logout</a>
-                </li>
+                                </Link>
+                            </li>
+
+                        </ul>
+                    })
+                }
+                <div className={styles.sidebottomlink}>
+                    <li >
+                        <IoLogOut size={"23"} />
+                        <a>Logout</a>
+                    </li>
+
+                </div>
 
             </div>
 
-        </div>
-        
 
         </div>
     )
