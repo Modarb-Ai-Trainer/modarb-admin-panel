f9c5d84604f95a4de3cf84915f9cc49754b827b5
Duaa Elsayed <73131139+doaa10@users.noreply.github.com>
2024-03-30 02:04:53 +0200

    modify the conflict of sideLinks

--- src/components/large/SideDrawer/NavLinks.tsx
+++ src/components/large/SideDrawer/NavLinks.tsx
@@ -9,34 +9,34 @@ import { IoLogOut } from "react-icons/io5";
 const NavLinks = () => {
     const pathname = usePathname();
     return (
-        <div className={styles.CenterLinks}>
-        <div className={styles.sidelinks}>
+        <div className={styles.centerLinks}>
+        <div className={styles.centerLinks_sideLinks}>
             {
                 links.map((link) => {
-                    return <ul key={link.id} >
+                    return <ul key={link.id} className={pathname === link.path ? styles.activeLink : styles.other}>
 
-                        <li className={pathname === link.path ? styles.activeLink : styles.other}>
+                        <li >
                             {link.icon}
                         
                             <Link href={link.path}>
                             {link.title}
 
-                                </Link>
-                            </li>
+                            </Link>
+                        </li>
 
                     </ul>
                 })
             }
-             <div className={styles.sidebottomlink}>
+             <div className={styles.centerLinks_sideLinks_sidebottomlink}>
                 <li >
                     <IoLogOut size={"23"} />
                     <a>Logout</a>
                 </li>
 
-                </div>
-
             </div>
 
+        </div>
+        
 
     </div>
     )
--- src/components/large/SideDrawer/SideDrawer.tsx
+++ src/components/large/SideDrawer/SideDrawer.tsx
@@ -1,21 +1,18 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './SideDrawer.module.css'
 import NavLinks from './NavLinks';
 import { RxHamburgerMenu } from "react-icons/rx";
 
 
 function SideDrawer() {
-    const [showMenu, setShowMenu] = useState(false);
 
-    const toggleMenu = () => {
-      setShowMenu(!showMenu);
-    };
 
 
     return (
         <div className={styles.SideDrawer}>
             <div className={styles.SideDrawer_sideContainer}>
+
             <div className={styles.SideDrawer_sideContainer_sideLogo}>
                 <img className={styles.SideDrawer_sideContainer_sideLogo_img} src="/images/logo.png"></img>
             </div>
