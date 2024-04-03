c0225a3538e60bbbca2a42b2228469dc17d2de10
Duaa Elsayed <73131139+doaa10@users.noreply.github.com>
2024-04-03 23:47:39 +0200

    modify sideDrawer responsiveness

--- src/components/large/SideDrawer/SideDrawer.tsx
+++ src/components/large/SideDrawer/SideDrawer.tsx
@@ -1,32 +1,33 @@
 'use client'
-import React from 'react'
-import styles from './SideDrawer.module.css'
+import React, { useState } from 'react';
+import styles from './SideDrawer.module.css';
 import NavLinks from './NavLinks';
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 
 
 function SideDrawer() {
+    const [showSideDrawer, setShowSideDrawer] = useState(false);
 
-
+    const toggleSideDrawer = () => {
+        setShowSideDrawer(!showSideDrawer);
+    };
 
     return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.SideDrawer_sideContainer}>
-
-            <div className={styles.SideDrawer_sideContainer_sideLogo}>
-                <img className={styles.SideDrawer_sideContainer_sideLogo_img} src="/images/logo.png"></img>
+        <div>
+            <div className={`${styles.BurgerMenuIcon} ${showSideDrawer && styles.active}`} onClick={toggleSideDrawer}>
+               {showSideDrawer ? <IoClose size={20}/> :  <RxHamburgerMenu size={20}/>} 
             </div>
-
-            
-            <NavLinks />
-           
-          
-           
-
+            <div className={`${styles.SideDrawer} ${showSideDrawer && styles.open}`}>
+                <div className={styles.SideDrawer_sideContainer}>
+                    <div className={styles.SideDrawer_sideContainer_sideLogo}>
+                        <img className={styles.SideDrawer_sideContainer_sideLogo_img} src="/images/logo.png" alt="Logo" />
+                    </div>
+                    <NavLinks />
+                </div>
             </div>
         </div>
-        
-    )
+    );
 }
 
-export default SideDrawer
+export default SideDrawer;
